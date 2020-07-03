71dea2b8212fef32e47077f2e20c5b8666af3fbf
chuanky <shicq01@gmail.com>
2020-07-03 16:16:45 +0800

    添加事件系统展示场景

---
+++ Assets/Scripts/EventSystemDemo/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text text;
+    public Text achievementText;
+
+    private int textValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textValue = 0;
+        text.text = textValue.ToString();
+        achievementText.enabled = false;
+        GameEvents.instance.OnNumberAchieved += OnGoalAchieved;
+        GameEvents.instance.OnNumberChanged += OnNumberChanged;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            textValue += 1;
+            GameEvents.instance.NumberChanged(textValue);
+            textValue = textValue % 10;
+            text.text = textValue.ToString();
+        }
+    }
+
+    private void OnGoalAchieved()
+    {
+        achievementText.enabled = true;
+    }
+
+    private void OnNumberChanged(int number)
+    {
+        Debug.Log("Number has changed to " + number);
+        if (number == 10)
+        {
+            GameEvents.instance.NumberAchieved();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.instance.OnNumberAchieved -= OnGoalAchieved;
+    }
+}
---
+++ Assets/Scripts/EventSystemDemo/GameEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public event Action OnNumberAchieved;
+
+    public void NumberAchieved()
+    {
+        OnNumberAchieved?.Invoke();
+    }
+
+    public event Action<int> OnNumberChanged;
+
+    public void NumberChanged(int number)
+    {
+        OnNumberChanged?.Invoke(number);
+    }
+}
